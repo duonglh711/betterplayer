a04e76f7b79b11847fb995146d38eab2ca052ffb
Duong Le <duonglh711@outlook.com>
2023-07-16 13:28:44 +0700

    Update better_player_dash_utils.dart

--- lib/src/dash/better_player_dash_utils.dart
+++ lib/src/dash/better_player_dash_utils.dart
@@ -50,8 +50,17 @@ class BetterPlayerDashUtils {
           int.parse(representation.getAttribute('height') ?? '0');
       final int bitrate =
           int.parse(representation.getAttribute('bandwidth') ?? '0');
-      final int frameRate =
-          int.parse(representation.getAttribute('frameRate') ?? '0');
+
+      int frameRate = 0;
+      final String? frameRateStr = representation.getAttribute('frameRate');
+      final arr = frameRateStr?.split('/');
+      if (arr?[1].isEmpty == false) {
+        frameRate =
+            (int.parse(arr?[0] ?? '0') / int.parse(arr?[1] ?? '1')).round();
+      } else {
+        frameRate = int.parse(frameRateStr ?? '0');
+      }
+
       final String? codecs = representation.getAttribute('codecs');
       final String? mimeType = MimeTypes.getMediaMimeType(codecs ?? '');
       tracks.add(BetterPlayerAsmsTrack(
